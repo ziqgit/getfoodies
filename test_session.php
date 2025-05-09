53dc9a28af01902697f6ce91ddc010074015c427
Haziqgit <mhaziqfizi.hazali@gmail.com>
2025-05-10 05:25:56 +0800

    Update test_session.php

--- test_session.php
+++ test_session.php
@@ -3,13 +3,6 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-session_set_cookie_params([
-    'httponly' => true,
-    'secure' => false,
-    'samesite' => 'Lax',
-    // 'domain' => '.getfoodies.website', // <-- comment this out for the test
-    'path' => '/',
-]);
 session_start();
 if (!isset($_SESSION['test'])) {
     $_SESSION['test'] = 'hello';
