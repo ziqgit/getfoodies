f9a14f1e043a0c84585a1256f95fe73a18814175
Haziqgit <mhaziqfizi.hazali@gmail.com>
2025-05-10 05:00:20 +0800

    Update logout.php

--- logout.php
+++ logout.php
@@ -2,6 +2,14 @@
 	<head><link rel="stylesheet" href="includes/loggedin.css" type="text/css" media="screen"/></head>
 </html>
 <?php
+session_set_cookie_params([
+    'httponly' => true,
+    'secure' => false, // Set to true if using HTTPS
+    'samesite' => 'Lax',
+    'domain' => '.getfoodies.website',
+    'path' => '/',
+]);
+ini_set('session.cookie_httponly', 1);
 // This page lets the user logout.
 // This version uses sessions.
 session_start(); // Access the existing session.
