03c7546054992c091f654bae126960e13013d8d2
Haziqgit <mhaziqfizi.hazali@gmail.com>
2025-05-10 04:14:57 +0800

    Update login_page.inc.php

--- includes/login_page.inc.php
+++ includes/login_page.inc.php
@@ -50,6 +50,7 @@ if (isset($_SESSION['errors']) && !empty($_SESSION['errors'])) {
 
 <div class="wrapper">
     <form class="form-signin" action="login.php" method="post">
+    <input type="hidden" name="csrf_token" value="<?php echo $_SESSION['csrf_token']; ?>">
         <h1 id="logo">Login</h1>
         <input type="text" class="form-control <?php echo !empty($email_error) ? 'error-border' : ''; ?>" placeholder="Email Address" name="email" size="20" maxlength="60" required/>
         <?php if (!empty($email_error)): ?>
