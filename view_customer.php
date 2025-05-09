23099662258cfb9efea771b75e4fe0e09ab8f58c
Haziqgit <mhaziqfizi.hazali@gmail.com>
2025-05-10 05:12:50 +0800

    Update view_customer.php

--- view_customer.php
+++ view_customer.php
@@ -3,7 +3,6 @@ session_set_cookie_params([
     'httponly' => true,
     'secure' => false, // Set to true if using HTTPS
     'samesite' => 'Lax',
-    'domain' => '.getfoodies.website',
     'path' => '/',
 ]);
 ini_set('session.cookie_httponly', 1);
