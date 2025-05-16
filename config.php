c09dcf70fd9db25f3e166e32c4e3f90d7b1a621e
Haziqgit <mhaziqfizi.hazali@gmail.com>
2025-05-16 08:27:00 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,24 +1,24 @@
 <?php
-// Define the Gmail smtp server
-define('MAILHOST', "smtp.gmail.com");
+// Define the SendGrid smtp server
+define('MAILHOST', "smtp.sendgrid.net");
  
-// Define as a username the email that you use in your Gmail account.
-define('USERNAME', "izzdanial25@gmail.com");
+// Define as a username the SendGrid API key name
+define('USERNAME', "apikey");
  
-// Define your 16 digit Gmail app-password.
-define('PASSWORD', "whhq jgsa jdxv jwch");
+// Define your SendGrid API key
+define('PASSWORD', "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");
  
 // Define the email address from which the email is sent.
 // This email belongs to my website, it has nothing to do with Gmail.
-define('SEND_FROM', "izzdanial23@gmail.com");
+define('SEND_FROM', "noreply@getfoodies.website");
  
 // Define the name of the website from which the email is sent. 
 define('SEND_FROM_NAME', "GetFoodies");
  
 // Define the reply-to address.
-define('REPLY_TO', "reply-to-izzdanial25@gmail.com");
+define('REPLY_TO', "noreply@getfoodies.website");
  
 // Define the reply-to name.
-define('REPLY_TO_NAME', "Imran Hakimi");
+define('REPLY_TO_NAME', "GetFoodies Team");
 
 ?>
