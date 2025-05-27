db71a049d86ccd6d5a703e247e3c8a14ce70b485
Haziqgit <mhaziqfizi.hazali@gmail.com>
2025-05-27 12:13:59 +0800

    Update staff_login_page.inc.php

--- includes/staff_login_page.inc.php
+++ includes/staff_login_page.inc.php
@@ -10,25 +10,18 @@ include ('includes/header.html');
 $email_error = $password_error = '';
 $lockout_message = '';
 $general_errors = [];
+$all_errors_to_display = [];
 
 if (isset($_SESSION['staff_errors']) && !empty($_SESSION['staff_errors'])) {
     foreach ($_SESSION['staff_errors'] as $msg) {
         if (strpos($msg, 'locked') !== false || strpos($msg, 'Too many failed login attempts from your IP address') !== false) {
             $lockout_message = $msg;
-        } elseif (strpos($msg, 'email') !== false) {
-            $email_error = $msg;
-        } elseif (strpos($msg, 'password') !== false) {
-            $password_error = $msg;
         } else {
-            // Add other non-lockout/email/password errors to a general errors array for display
-            $general_errors[] = $msg;
+            $all_errors_to_display[] = $msg;
         }
     }
     unset($_SESSION['staff_errors']);
 }
-
-// Combine email, password, and general errors for the main error display
-$all_errors = array_merge(array_filter([$email_error, $password_error]), $general_errors ?? []);
 ?>
 <div class="wrapper">
     <?php if (!empty($lockout_message)): ?>
@@ -41,22 +34,22 @@ $all_errors = array_merge(array_filter([$email_error, $password_error]), $genera
         <h1 id="logo">Staff Login</h1>
         <input type="text" class="form-control <?php echo !empty($email_error) ? 'error-border' : ''; ?>" placeholder="Email Address" name="email" size="20" maxlength="60" required/>
         <?php if (!empty($email_error)): ?>
-            <div class="error"><?php echo $email_error; ?></div>
+            <!-- Removed specific email error display here to avoid duplication -->
         <?php endif; ?>
         <input type="password" class="form-control <?php echo !empty($password_error) ? 'error-border' : ''; ?>" placeholder="Password" name="pass1" size="20" maxlength="60" required/>
         <?php if (!empty($password_error)): ?>
-            <div class="error"><?php echo $password_error; ?></div>
+            <!-- Removed specific password error display here to avoid duplication -->
         <?php endif; ?>
         <p><input class="btn btn-lg btn-primary btn-block" type="submit" name="submit" value="Login" /></p>
     </form>
 </div>
 <div class="background-slider"></div>
 <?php 
-// Display any error messages:
-if (isset($all_errors) && !empty($all_errors)) {
+// Display any collected error messages:
+if (isset($all_errors_to_display) && !empty($all_errors_to_display)) {
     echo '<h1 id="erorrh1">Error!</h1>
     <p class="errorp">The following error(s) occurred:<br />';
-    foreach ($all_errors as $msg) {
+    foreach ($all_errors_to_display as $msg) {
         echo " - $msg<br />\n";
     }
     echo '</p><p>Please try again.</p>';
