ac7737bc91b37cb0fb6e9bf1bf0abccf6b73da7b
Haziqgit <mhaziqfizi.hazali@gmail.com>
2025-05-29 00:29:38 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 header('X-Frame-Options: DENY');
- header("Content-Security-Policy: default-src 'self'; script-src 'self'; object-src 'none' frame-ancestors 'none'; form-action 'self'; base-uri 'self';");
+header("Content-Security-Policy: default-src 'self'; script-src 'self'; style-src 'self' https://maxcdn.bootstrapcdn.com; img-src 'self'; font-src 'self' https://maxcdn.bootstrapcdn.com; object-src 'none'; frame-ancestors 'none'; form-action 'self'; base-uri 'self';");
 
         $page_title = 'Homepage';
         include ('includes/header.html');
